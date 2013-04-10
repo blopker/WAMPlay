d244de94a8dccf41ed5566f86bfe613e254c50d7
blopker <lopkerk@gmail.com>
2013-04-09 18:25:21 -0700

    fixed publish script

--- module/publish.sh
+++ module/publish.sh
@@ -5,7 +5,8 @@ play publish-local
 
 cd ~/code
 
-if [ !-d blopker.github.com ]; then
+if [ ! -d blopker.github.com ]; then
+  echo "HI"
   git clone git@github.com:blopker/blopker.github.com.git
 fi
 
