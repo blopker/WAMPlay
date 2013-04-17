40a5ea8c0326c10a9cd094978354e8d63ad216a4
blopker <lopkerk@gmail.com>
2013-04-16 20:24:52 -0700

    updated rpc example

--- sample/rpc/app/controllers/RPC.java
+++ sample/rpc/app/controllers/RPC.java
@@ -2,21 +2,20 @@ package controllers;
 
 import org.codehaus.jackson.JsonNode;
 
-import com.blopker.wamplay.annotations.ControllerURIPrefix;
+import com.blopker.wamplay.annotations.URIPrefix;
 import com.blopker.wamplay.annotations.onRPC;
 import com.blopker.wamplay.controllers.WAMPlayContoller;
-import com.blopker.wamplay.models.WAMPlayClient;
 
-@ControllerURIPrefix("http://example.com/calc#")
+@URIPrefix("http://example.com/calc#")
 public class RPC extends WAMPlayContoller {
 
 	@onRPC("meaningOfLife")
-	public static String getMeaningOfLife(WAMPlayClient client) {
+	public static String getMeaningOfLife(String sessionID) {
 		return "Meaning of life is: 42";
 	}
 
 	@onRPC("capital")
-	public static String add(WAMPlayClient client, JsonNode[] args) {
+	public static String add(String sessionID, JsonNode[] args) {
 		if (!args[0].isTextual() || args[0].isNumber()) {
 			throw new IllegalArgumentException("Argument is not a word!");
 		}
--- sample/rpc/project/Build.scala
+++ sample/rpc/project/Build.scala
@@ -7,13 +7,13 @@ object ApplicationBuild extends Build {
   val appName         = "rpc"
   val appVersion      = "1.0-SNAPSHOT"
 
-val appDependencies = Seq(
-    javaCore,
-    "com.blopker" %% "wamplay" % "0.0.3"
-  )
+  val appDependencies = Seq(
+      javaCore,
+      "com.blopker" %% "wamplay" % "0.0.4"
+    )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    resolvers += Resolver.url("Bo's Repository", url("http://blopker.github.com/releases/"))(Resolver.ivyStylePatterns)
+    resolvers ++= Seq("Bo's Repository" at "http://blopker.github.com/maven-repo/")
   )
 
 }
