f76920fcd5430585ba6574731f7f3d14dcc29cc0
blopker <lopkerk@gmail.com>
2013-04-17 11:26:22 -0700

    update publish script

--- module/publish.sh
+++ module/publish.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 play clean
-play publish-local
+play publish
 
 cd ~/code
 
 if [ ! -d blopker.github.com ]; then
-  echo "HI"
   git clone git@github.com:blopker/blopker.github.com.git
 fi
 
-cp -rv ~/play/repository/local/com.blopker blopker.github.com/releases
+#cp -rv ~/play/repository/local/com.blopker blopker.github.com/releases
 
-cd blopker.github.com
+cd ~/code/blopker.github.com
 git add --all :/
 git commit -am "Updated releases"
 git push origin master
